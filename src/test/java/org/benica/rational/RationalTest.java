de96dd1949b6c901bc6939fb0bd38ca250e19210
wbenica <wbenica@calpoly.edu>
2018-12-22 17:46:59 -0800

    rational implements comparable

--- src/test/java/org/benica/rational/RationalTest.java
+++ src/test/java/org/benica/rational/RationalTest.java
@@ -14,6 +14,10 @@ public class RationalTest {
         Rational rhs = new Rational ( 2, 16 );
         Rational res = new Rational ( 7, 8 );
         assertEquals ( lhs.plus ( rhs ), res );
+
+        // plus integer
+        Rational test = new Rational ( 0, 4 );
+        assertEquals ( test.plus ( 2 ), new Rational ( 2 ) );
     }
 
     @Test
@@ -67,11 +71,17 @@ public class RationalTest {
         Rational lhs = new Rational ( 5, 25 );
         Rational rhs = new Rational ( 1, 5 );
         assertEquals ( lhs, rhs );
+
+        // equals self
         assertEquals ( lhs, lhs );
         Rational badTest = new Rational ( 2, 5 );
         assertNotEquals ( badTest, rhs );
 
+        // rational and integer
         assertEquals ( new Rational ( 4, 2 ), ( Object ) 2 );
+
+        // rational and not rational/integer
+        assertNotEquals ( lhs, "What" );
     }
 
     @Test
@@ -79,13 +89,6 @@ public class RationalTest {
 
         Rational test = new Rational ( 5, 25 );
         assertEquals ( 105, test.hashCode ( ) );
-    }
-
-    @Test
-    public void plus1 ( ) {
-
-        Rational test = new Rational ( 0, 4 );
-        assertEquals ( test.plus ( 2 ), new Rational ( 2 ) );
     }
 
     @Test
@@ -116,9 +119,23 @@ public class RationalTest {
         assertEquals ( 502, test.hashCode ( ) );
     }
 
+    @Test
+    public void compareToTest ( ) {
+
+        Rational lhs = new Rational ( 31, 5 );
+        Rational rhs = new Rational ( 23, 107 );
+        assertEquals ( true, lhs.compareTo ( rhs ) > 0 );
+    }
+
     @Test( expected = IllegalArgumentException.class )
     public void zeroDenominatorTest ( ) {
 
         Rational test = new Rational ( 5, 0 );
+    }
+
+    @Test( expected = IllegalArgumentException.class )
+    public void gcdZeroZero ( ) {
+
+        Rational.gcd ( 0, 0 );
     }
 }
